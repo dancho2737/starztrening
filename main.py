acaa65b4d1206feb8048e0d2b6491e8394e6eef5
dancho2737 <churocov9702@gmail.com>
2025-11-29 15:06:31 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from handlers.commands import router as commands_router
 from handlers.messages import router as messages_router
 
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-TOKEN = os.getenv("TOKEN")
+# Читаем токены из переменных окружения Heroku
+TOKEN = os.getenv("BOT_TOKEN")
+OPENAI_KEY = os.getenv("OPENAI_KEY")  # если будешь использовать в AI-ответах
 
-bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
-# Подключаем все роутеры
+# Подключаем роутеры
 dp.include_router(commands_router)
 dp.include_router(messages_router)
-
 
 async def main():
     print("Бот запущен.")
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
