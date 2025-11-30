b5a994d2d4bc1fd2f83899886615ca736b7fe066
dancho2737 <churocov9702@gmail.com>
2025-11-30 12:11:07 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,8 @@
+# bot/config.py
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN") or os.getenv("TOKEN")
 OPENAI_KEY = os.getenv("OPENAI_KEY")
