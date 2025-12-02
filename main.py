1e0e1f9916aad7aa0852572f13d452895d32071d
dancho2737 <churocov9702@gmail.com>
2025-12-02 17:51:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,21 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
 from bot.config import BOT_TOKEN
-from handlers import commands, messages, callbacks
+from handlers import messages, commands
 
 bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
 
-def register_handlers():
-    dp.include_router(commands.router)
-    dp.include_router(messages.router)
-    dp.include_router(callbacks.router)
-
-async def on_startup():
-    await bot.set_my_commands(
-        [
-            BotCommand(command="start", description="Запустить бота"),
-            BotCommand(command="help", description="Помощь"),
-        ]
-    )
+# Регистрируем хендлеры
+dp.include_router(commands.router)
+dp.include_router(messages.router)
 
 async def main():
-    register_handlers()
-    await on_startup()
-    # Long polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_my_commands([
+        BotCommand(command="start", description="Запуск бота"),
+        BotCommand(command="help", description="Помощь"),
+    ])
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
